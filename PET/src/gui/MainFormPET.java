0d52cdf8a3c07b4bfcd857837524f45c912422eb
Aleo <Aleo@aleo-PC>
2012-12-23 18:47:42 +0200

    Добавлен макет просмотра, редактирования факультета Signed-off-by:Aleo <Aleo@aleo-PC>

--- PET/src/gui/MainFormPET.java
+++ PET/src/gui/MainFormPET.java
@@ -4,6 +4,8 @@
  */
 package gui;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author Aleo
@@ -26,7 +28,61 @@ public class MainFormPET extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuDictionaries = new javax.swing.JMenu();
+        jMenuItemDepartment = new javax.swing.JMenuItem();
+        jMenuItemFaculty = new javax.swing.JMenuItem();
+        jMenuItemSubject = new javax.swing.JMenuItem();
+        jMenuItemTeachers = new javax.swing.JMenuItem();
+        jMenuItemMark = new javax.swing.JMenuItem();
+        jMenuItemTypeWork = new javax.swing.JMenuItem();
+        jMenuItemGroup = new javax.swing.JMenuItem();
+        jMenuItemStudent = new javax.swing.JMenuItem();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("PET");
+        setLocationByPlatform(true);
+        setMinimumSize(new java.awt.Dimension(40, 40));
+        setModalExclusionType(java.awt.Dialog.ModalExclusionType.TOOLKIT_EXCLUDE);
+
+        jMenu1.setText("File");
+        jMenuBar1.add(jMenu1);
+
+        jMenuDictionaries.setText("Словари");
+
+        jMenuItemDepartment.setText("Факультеты");
+        jMenuItemDepartment.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItemDepartmentActionPerformed(evt);
+            }
+        });
+        jMenuDictionaries.add(jMenuItemDepartment);
+
+        jMenuItemFaculty.setText("Деканаты");
+        jMenuDictionaries.add(jMenuItemFaculty);
+
+        jMenuItemSubject.setText("Предметы");
+        jMenuDictionaries.add(jMenuItemSubject);
+
+        jMenuItemTeachers.setText("Преподаватели");
+        jMenuDictionaries.add(jMenuItemTeachers);
+
+        jMenuItemMark.setText("Оценки");
+        jMenuDictionaries.add(jMenuItemMark);
+
+        jMenuItemTypeWork.setText("Типы оценок");
+        jMenuDictionaries.add(jMenuItemTypeWork);
+
+        jMenuItemGroup.setText("Группы");
+        jMenuDictionaries.add(jMenuItemGroup);
+
+        jMenuItemStudent.setText("Студенты");
+        jMenuDictionaries.add(jMenuItemStudent);
+
+        jMenuBar1.add(jMenuDictionaries);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -36,11 +92,17 @@ public class MainFormPET extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 279, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItemDepartmentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemDepartmentActionPerformed
+        DepartmentFrame df = new DepartmentFrame();
+        df.setLocationByPlatform(true);
+        df.setVisible(true);
+    }//GEN-LAST:event_jMenuItemDepartmentActionPerformed
 
     /**
      * @param args the command line arguments
@@ -52,19 +114,14 @@ public class MainFormPET extends javax.swing.JFrame {
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainFormPET.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainFormPET.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainFormPET.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+//                if ("Nimbus".equals(info.getName())) {
+//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+//                    break;
+//                }
+//            }
+            javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(MainFormPET.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
@@ -72,10 +129,24 @@ public class MainFormPET extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new MainFormPET().setVisible(true);
+                MainFormPET mf = new MainFormPET();
+                mf.setExtendedState(JFrame.MAXIMIZED_BOTH);
+                
+                mf.setVisible(true);
             }
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenu jMenuDictionaries;
+    private javax.swing.JMenuItem jMenuItemDepartment;
+    private javax.swing.JMenuItem jMenuItemFaculty;
+    private javax.swing.JMenuItem jMenuItemGroup;
+    private javax.swing.JMenuItem jMenuItemMark;
+    private javax.swing.JMenuItem jMenuItemStudent;
+    private javax.swing.JMenuItem jMenuItemSubject;
+    private javax.swing.JMenuItem jMenuItemTeachers;
+    private javax.swing.JMenuItem jMenuItemTypeWork;
     // End of variables declaration//GEN-END:variables
 }
