69d238f7c28fca4baa4293f3092cd6842d591903
Aleo <72aleo72@gmail.com>
2013-01-20 21:06:21 +0200

    Добавлен интерфейс
    Этот интерфейс должны реализовывать все сущности которые созданы для
    обмена данных с базой данны�

---
+++ PET/src/database/entity/EntryDataBase.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database.entity;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Aleo
+ */
+public interface EntryDataBase {
+    
+    /**
+     * Метод добавляет в базу данных данный объект 
+     * @return код результата: -1 - Такие данные уже есть
+     *                         -2 - Произошла вообще ошибка
+     *                         Или количество добавленых записей 
+     * @throws SQLException 
+     */
+    public int insertInto() throws SQLException;
+    
+    /**
+     * Метод заносит измененный объект в базу данных.
+     * Тоесть данный элимент в базу занесеть переданый элимент
+     * @param объект на который надо заменить
+     * @return код результата: -1 - Такие данные уже есть
+     *                         -2 - Произошла вообще ошибка
+     *                         Или количество добавленых записей
+     * @throws SQLException 
+     */
+    public int updateTable(Teacher newTeacher) throws SQLException;
+}
