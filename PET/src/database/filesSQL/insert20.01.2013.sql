0791d117bf8a2f4b3ff08b0fbd0d797d1fc6d9c8
Aleo <72aleo72@gmail.com>
2013-01-20 13:06:35 +0200

    Добавлен файл с insert типов тестов

---
+++ PET/src/database/filesSQL/insert20.01.2013.sql
@@ -0,0 +1,36 @@
+insert into Question_type(
+	name_question_type, 
+	description_question_type,
+	name_prog_question_type
+	) values (
+	'Выбор правильных вариантов ответа',
+	'В этом типе заданий необходимо выбрать один или несколько правильных утверждений',
+	'Selected'
+	);
+insert into Question_type(
+	name_question_type, 
+	description_question_type,
+	name_prog_question_type
+	) values (
+	'Подтверждение итиности',
+	'В этом типе заданий необходимо согласиться или не согласиться с утверждением',
+	'AlternativeChoice'
+	);
+insert into Question_type(
+	name_question_type, 
+	description_question_type,
+	name_prog_question_type
+	) values (
+	'Указать правильную последовательность',
+	'В этом типе заданий необходимо указать правильную последовательность фраз',
+	'Sequencing'
+	);
+insert into Question_type(
+	name_question_type, 
+	description_question_type,
+	name_prog_question_type
+	) values (
+	'Указать правильные соответствия',
+	'В этом типе заданий необходимо указать правильные соответствия',
+	'Matched'
+	);
