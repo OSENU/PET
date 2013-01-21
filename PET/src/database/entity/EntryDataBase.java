4969925e8d797129961348207952aea7f6014b6a
Алексей <72aleo72@gmail.com>
2013-01-21 17:48:01 +0200

    Update PET/src/database/entity/EntryDataBase.java
    Изменен комментарий

--- PET/src/database/entity/EntryDataBase.java
+++ PET/src/database/entity/EntryDataBase.java
@@ -1,6 +1,7 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Интерфейс позволяющий объеденить все сущности базы данных
+ * данный интерфейс говорит о том, что любая сущность должна 
+ * себя добавить в базу, или обновить
  */
 package database.entity;
 
